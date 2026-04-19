52a513f03ef18ca97e4bb673cc306a9c67ed56a2
Jimmy Yao <dev.jimm.py@gmail.com>
2026-04-19 11:33:11 -0500

    refactor: drop unused package re-export shims

--- src/meridian/lib/launch/streaming/__init__.py
+++ src/meridian/lib/launch/streaming/__init__.py
@@ -1,14 +1,1 @@
-"""Streaming launch submodules."""
-
-from meridian.lib.launch.streaming.decision import (
-    TerminalEventOutcome,
-    terminal_event_outcome,
-)
-from meridian.lib.launch.streaming.heartbeat import FileHeartbeat, HeartbeatTouch
-
-__all__ = [
-    "FileHeartbeat",
-    "HeartbeatTouch",
-    "TerminalEventOutcome",
-    "terminal_event_outcome",
-]
+"""Streaming launch internals."""
--- src/meridian/lib/state/spawn/__init__.py
+++ src/meridian/lib/state/spawn/__init__.py
@@ -1,6 +1,1 @@
-"""Spawn state projection helpers."""
-
-from meridian.lib.state.spawn.events import reduce_events
-from meridian.lib.state.spawn.repository import FileSpawnRepository, SpawnRepository
-
-__all__ = ["FileSpawnRepository", "SpawnRepository", "reduce_events"]
+"""Spawn state internals."""
